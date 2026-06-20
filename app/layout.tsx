fc80123b36eb45bfaf981e66153b0ff45eaf7012
TendoArisu <165495758+XUANHLGG@users.noreply.github.com>
2026-06-20 19:43:08 +0800

    依旧依旧解决冲突

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh-CN" className={inter.className} suppressHydrationWarning>
-      <body class="flex flex-col min-h-screen">
+      <body className="flex flex-col min-h-screen">
         <Provider>
           <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
             {children}
