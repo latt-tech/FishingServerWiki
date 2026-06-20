ac8fcdf2e1662684ba8396bb8c556d225f5d7c3e
TendoArisu <165495758+XUANHLGG@users.noreply.github.com>
2026-06-20 19:22:39 +0800

    修改根页面

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,23 @@
 import { Inter } from 'next/font/google';
 import { Provider } from '@/components/provider';
 import './global.css';
+import { source } from '@/lib/source';
+import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { baseOptions } from '@/lib/layout.shared';
 
 const inter = Inter({
   subsets: ['latin'],
 });
 
-export default function Layout({ children }: LayoutProps<'/'>) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
-        <Provider>{children}</Provider>
+    <html lang="zh-CN" className={inter.className} suppressHydrationWarning>
+      <body class="flex flex-col min-h-screen">
+        <Provider>
+          <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+            {children}
+          </DocsLayout>
+        </Provider>
       </body>
     </html>
   );
--- lib/shared.ts
+++ lib/shared.ts
@@ -1,11 +1,4 @@
-export const appName = 'My App';
-export const docsRoute = '/docs';
-export const docsImageRoute = '/og/docs';
-export const docsContentRoute = '/llms.mdx/docs';
-
-// fill this with your actual GitHub info, for example:
-export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
-  branch: 'main',
-};
+export const appName = 'Fishing Server Wiki';
+export const docsRoute = '/';
+export const docsImageRoute = '/og';
+export const docsContentRoute = '/llms.mdx';
